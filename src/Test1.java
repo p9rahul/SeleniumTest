c7439483614809912619f6727b87bfaf858f4d01
Rahul Kumar <p9rahul@gmail.com>
2015-12-18 23:37:25 +0530

    branch 3
    delete and add deepak

--- src/Test1.java
+++ src/Test1.java
@@ -7,12 +7,7 @@ public class Test1 {
 		System.out.println("Hi Alok");
 		System.out.println("Hi Rajesh");
 		System.out.println("Hi Rakesh");
-		
-		//------------------
-		System.out.println("Hi Rahul");
-		System.out.println("Hi Alok");
-		System.out.println("Hi Rajesh");
-		System.out.println("Hi Rakesh");
+		System.out.println("Hi Deepak");
 	}
 
 }
