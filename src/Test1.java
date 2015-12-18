9296a5973bcfc3e0ccd6cbc4fdd26f8397fcb5a2
Rahul Kumar <p9rahul@gmail.com>
2015-12-18 20:17:01 +0530

    second commit

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Rahul");
+		System.out.println("Hi Alok");
+		System.out.println("Hi Rajesh");
+		System.out.println("Hi Rakesh");
+	}
+
+}
